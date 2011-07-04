e1c89f5f906519e6c83d8beeccd2feae78e42db4
nikolay <nikolay@semov.net>
2011-07-04 13:51:36 -0400

    Copyright Notice

--- RADAR/Conversion.vb
+++ RADAR/Conversion.vb
@@ -1,4 +1,19 @@
-﻿Friend Class Conversion
+﻿'Copyright (C) 2008-2011  Nikolay Semov
+
+'    This program is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Friend Class Conversion
     Friend Shared Function ConvertToString(ByRef bytes() As Byte) As String
         Dim k As New System.Text.StringBuilder
         Dim i As Integer
--- RADAR/RADIUSPacket.vb
+++ RADAR/RADIUSPacket.vb
@@ -1,4 +1,19 @@
-﻿Imports System.Net
+﻿'Copyright (C) 2008-2011  Nikolay Semov
+
+'    This program is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports System.Net
 Imports System.Security.Cryptography
 Imports RADAR.Conversion
 
